3bf503dc9aec07467e53ae1c5291e6ab44a6ae5f
terrencepledger <terrencepledger@gmail.com>
2025-07-25 18:05:57 -0500

    Fix ShareSheet for non-UIKit platforms

--- RoomRoster/Views/Components/ShareSheet.swift
+++ RoomRoster/Views/Components/ShareSheet.swift
@@ -1,8 +1,16 @@
+//
+//  ShareSheet.swift
+//  RoomRoster
+//
+//  Created by Terrence Pledger on 7/25/25.
+//
+
 import SwiftUI
+
 #if canImport(UIKit)
 import UIKit
-#endif
 
+/// A wrapper around ``UIActivityViewController`` for presenting the system share sheet on iOS and iPadOS.
 struct ShareSheet: UIViewControllerRepresentable {
     let activityItems: [Any]
 
@@ -13,3 +21,18 @@ struct ShareSheet: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
     }
 }
+#else
+/// ``ShareSheet`` is unavailable on platforms without UIKit. ``ShareLink`` can be used instead
+/// where supported.
+struct ShareSheet: View {
+    let activityItems: [Any]
+
+    var body: some View {
+        if #available(macOS 13, *) {
+            if let first = activityItems.first as? any Transferable {
+                ShareLink(item: first)
+            }
+        }
+    }
+}
+#endif
