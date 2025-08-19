b3a68879db55b5b8e4dfc7226fe223519d414262
terrencepledger <terrencepledger@gmail.com>
2025-08-19 15:03:29 -0500

    Refine legacy tag scanning

--- RoomRoster/Views/BarcodeScannerView.swift
+++ RoomRoster/Views/BarcodeScannerView.swift
@@ -21,14 +21,15 @@ struct BarcodeScannerView: UIViewControllerRepresentable {
     final class Coordinator: NSObject, AVCaptureMetadataOutputObjectsDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
         let onScan: (String) -> Void
         private var didScan = false
-        private var legacyScanRequested = false
+        private var legacyBoostFrames = 0
+        private var frameCounter = 0
 
         init(onScan: @escaping (String) -> Void) {
             self.onScan = onScan
         }
 
-        func requestLegacyScan() {
-            legacyScanRequested = true
+        func boostLegacyScan() {
+            legacyBoostFrames = 60
         }
 
         func metadataOutput(
@@ -50,11 +51,13 @@ struct BarcodeScannerView: UIViewControllerRepresentable {
             didOutput sampleBuffer: CMSampleBuffer,
             from connection: AVCaptureConnection
         ) {
-            guard !didScan, legacyScanRequested,
+            guard !didScan,
                   let buffer = CMSampleBufferGetImageBuffer(sampleBuffer)
             else { return }
 
-            legacyScanRequested = false
+            frameCounter += 1
+            if legacyBoostFrames <= 0 && frameCounter % 15 != 0 { return }
+            if legacyBoostFrames > 0 { legacyBoostFrames -= 1 }
 
             let request = VNRecognizeTextRequest { [weak self] request, error in
                 guard
@@ -66,16 +69,24 @@ struct BarcodeScannerView: UIViewControllerRepresentable {
                 for observation in results {
                     guard let candidate = observation.topCandidates(1).first else { continue }
                     let text = candidate.string.trimmingCharacters(in: .whitespacesAndNewlines)
-                    if text.range(of: "^A\\d{4}$", options: .regularExpression) != nil {
+                    if candidate.confidence > 0.8,
+                       text.range(of: "^A\\d{4}$", options: .regularExpression) != nil {
                         self.didScan = true
                         self.onScan(text)
                         break
                     }
                 }
             }
-            request.recognitionLevel = .fast
+            request.recognitionLevel = legacyBoostFrames > 0 ? .accurate : .fast
+            request.minimumTextHeight = 0.05
+            request.recognitionLanguages = ["en-US"]
+            request.usesLanguageCorrection = false
 
-            let handler = VNImageRequestHandler(cvPixelBuffer: buffer, orientation: .up, options: [:])
+            let handler = VNImageRequestHandler(
+                cvPixelBuffer: buffer,
+                orientation: CGImagePropertyOrientation(UIDevice.current.orientation),
+                options: [:]
+            )
             try? handler.perform([request])
         }
     }
@@ -83,6 +94,7 @@ struct BarcodeScannerView: UIViewControllerRepresentable {
     final class ScannerViewController: UIViewController {
         var coordinator: Coordinator?
         private let session = AVCaptureSession()
+        private var preview: AVCaptureVideoPreviewLayer?
 
         override func viewDidLoad() {
             super.viewDidLoad()
@@ -108,6 +120,14 @@ struct BarcodeScannerView: UIViewControllerRepresentable {
             preview.videoGravity = .resizeAspectFill
             preview.frame = view.layer.bounds
             view.layer.addSublayer(preview)
+            self.preview = preview
+
+            NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(orientationChanged),
+                name: UIDevice.orientationDidChangeNotification,
+                object: nil
+            )
 
             let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
             doubleTap.numberOfTapsRequired = 2
@@ -117,6 +137,7 @@ struct BarcodeScannerView: UIViewControllerRepresentable {
         override func viewDidAppear(_ animated: Bool) {
             super.viewDidAppear(animated)
             session.startRunning()
+            updatePreviewOrientation()
         }
 
         override func viewWillDisappear(_ animated: Bool) {
@@ -124,8 +145,54 @@ struct BarcodeScannerView: UIViewControllerRepresentable {
             session.stopRunning()
         }
 
+        override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            preview?.frame = view.bounds
+            updatePreviewOrientation()
+        }
+
+        deinit {
+            NotificationCenter.default.removeObserver(self)
+        }
+
         @objc private func handleDoubleTap() {
-            coordinator?.requestLegacyScan()
+            coordinator?.boostLegacyScan()
+        }
+
+        @objc private func orientationChanged() {
+            updatePreviewOrientation()
+        }
+
+        private func updatePreviewOrientation() {
+            guard let connection = preview?.connection,
+                  connection.isVideoOrientationSupported,
+                  let videoOrientation = AVCaptureVideoOrientation(deviceOrientation: UIDevice.current.orientation)
+            else { return }
+            connection.videoOrientation = videoOrientation
+        }
+    }
+}
+
+private extension AVCaptureVideoOrientation {
+    init?(deviceOrientation: UIDeviceOrientation) {
+        switch deviceOrientation {
+        case .portrait: self = .portrait
+        case .portraitUpsideDown: self = .portraitUpsideDown
+        case .landscapeLeft: self = .landscapeRight
+        case .landscapeRight: self = .landscapeLeft
+        default: return nil
+        }
+    }
+}
+
+private extension CGImagePropertyOrientation {
+    init(_ deviceOrientation: UIDeviceOrientation) {
+        switch deviceOrientation {
+        case .portrait: self = .right
+        case .portraitUpsideDown: self = .left
+        case .landscapeLeft: self = .up
+        case .landscapeRight: self = .down
+        default: self = .right
         }
     }
 }
